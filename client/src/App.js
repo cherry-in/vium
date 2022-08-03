f9bae683d5b66b08bf083f987720569f876ed811
pilmalion114 <pilmalion114@naver.com>
2022-08-03 15:14:13 +0900

    3번째 업데이트/장바구니,주문기록

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,12 @@ import Restaurant from "./Pages/Restaurant";
 import Restaurant_Pickup from "./Pages/Restaurant_Pickup";
 import Detail_Restaurant from "./Pages/Detail_Restaurant";
 import Zzim from "./Pages/Zzim";
+import Order_Report from "./Pages/Order_Report";
+import Jangbaguni_Nofood from "./Pages/Jangbaguni_Nofood";
+import Jangbaguni_withfood_delivery from "./Pages/Jangbaguni_withfood_delivery";
+import Jangbaguni_withfood_pickup from "./Pages/Jangbaguni_withfood_pickup";
+import Jangbaguni_Pay_delivery from "./Pages/Jangbaguni_Pay_delivery";
+import Jangbaguni_Pay_pickup from "./Pages/Jangbaguni_Pay_pickup";
 
 function App() {
   let element = useRoutes([
@@ -21,6 +27,12 @@ function App() {
     {path: '/Restaurant_Pickup', element: <Restaurant_Pickup />},
     {path: '/Detail_Restaurant', element: <Detail_Restaurant/>},
     {path: '/Zzim', element: <Zzim/>},
+    {path: '/Order_Report', element: <Order_Report/>},
+    {path: '/Jangbaguni_Nofood', element: <Jangbaguni_Nofood/>},
+    {path: '/Jangbaguni_withfood_delivery', element: <Jangbaguni_withfood_delivery/>},
+    {path: '/Jangbaguni_withfood_pickup', element: <Jangbaguni_withfood_pickup/>},
+    {path: '/Jangbaguni_Pay_delivery', element: <Jangbaguni_Pay_delivery/>},
+    {path: '/Jangbaguni_Pay_pickup', element: <Jangbaguni_Pay_pickup/>},
   ]);
 
   return element;
