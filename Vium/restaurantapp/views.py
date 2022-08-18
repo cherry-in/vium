366c9ea8a6f303dbb6286f534a6ad28984fff9d0
chuuuo <shchu07@korea.ac.kr>
2022-08-18 11:55:45 +0900

    fiX

--- Vium/restaurantapp/views.py
+++ Vium/restaurantapp/views.py
@@ -18,22 +18,11 @@ class MenuViewSet(mixins.RetrieveModelMixin, GenericViewSet):
     serializer_class = MenuDetailForm
     permission_classes = [AllowAny]
 
-class RestaurantFilter(filters.FilterSet):
-    payment_methods = filters.CharFilter(lookup_expr='icontains')
-    categories = filters.CharFilter(lookup_expr='icontains')
-
-    class Meta:
-        model = Restaurant
-        fields = ['payment_methods', 'categories']
-
-
 class RestaurantViewSet(ReadOnlyModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantListForm
     filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
-    filterset_class = RestaurantFilter
-    ordering_fields = ['average_rating', 'delivery_charge', 'min_order_price', 'delivery_time', 'review_count',
-                       'owner_comment_count']
+    ordering_fields = ['reorder_count', 'delivery_charge', 'min_order_price', 'delivery_time']
     ordering = ('id',)
     permission_classes = [AllowAny]
     HOME_VIEW_PAGE_SIZE = 20
@@ -49,63 +38,14 @@ class RestaurantViewSet(ReadOnlyModelViewSet):
             return RestaurantDetailForm
         return super().get_serializer_class()
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        qs = self.filter_by_distance_manual(qs)
-        qs = self.filter_by_search(qs)
-        if self.action == 'list':
-            return qs.prefetch_related('bookmark')
-        if self.action == 'retrieve':
-            return qs.prefetch_related('menu_group__menu')
-
-    def filter_by_search(self, qs):
-        search = self.request.query_params.get('search')
-        if search:
-            qs = Restaurant.objects.filter(Q(name__icontains=search) |
-                                           Q(menu_group__menu__name__icontains=search) |
-                                           Q(tags__name__icontains=search)).distinct()
-        return qs
-
-    def filter_by_distance_manual(self, qs):
-        data = self.request.query_params
-        if self.action == 'list':
-            lat = data.get('lat')
-            lng = data.get('lng')
-            if lat and lng:
-                lat = float(lat)
-                lng = float(lng)
-                min_lat = lat - 0.009
-                max_lat = lat + 0.009
-                min_lon = lng - 0.015
-                max_lon = lng + 0.01
-
-                qs = qs.filter(lat__gte=min_lat, lat__lte=max_lat,
-                               lng__gte=min_lon, lng__lte=max_lon)
-        return qs
-
     @action(detail=False, methods=['GET'])
-    def home_view_average_rating(self, request, *args, **kwargs):
-        qs = self.get_queryset().order_by('-average_rating').filter(average_rating__gte=4)
-        return self.home_view_results(qs)
-
-    @action(detail=False, methods=['GET'])
-    def home_view_bookmark(self, request, *args, **kwargs):
-        qs = self.get_queryset().order_by('-bookmark')
-        return self.home_view_results(qs)
-
-    @action(detail=False, methods=['GET'])
-    def home_view_delivery_discount(self, request, *args, **kwargs):
-        qs = self.get_queryset().filter(delivery_discount__gt=0)
+    def home_view_reorder_count(self, request, *args, **kwargs):
+        qs = self.get_queryset().order_by('-reorder_count').filter(reorder_count__gte=4)
         return self.home_view_results(qs)
 
     @action(detail=False, methods=['GET'])
     def home_view_delivery_charge(self, request, *args, **kwargs):
         qs = self.get_queryset().filter(delivery_charge=0)
-        return self.home_view_results(qs)
-
-    @action(detail=False, methods=['GET'])
-    def home_view_review(self, request, *args, **kwargs):
-        qs = self.get_queryset().order_by('-review_count')
         return self.home_view_results(qs)
 
     @action(detail=False, methods=['GET'])
